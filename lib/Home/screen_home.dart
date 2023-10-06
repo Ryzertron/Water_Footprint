4f0fc8a546e6a3b2b1194f01bb68daa8f518cf06
GOKULKRISHNA V <gkmail2004@gmail.com>
2023-10-06 13:30:20 +0530

    made seperate working directory developers.

---
+++ lib/Home/screen_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+    //TODO: Implement Home Page here. 
+    //Any custom widgets should be written in a seperate file.
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:byte_brawlers/screen_home.dart';
+import 'package:byte_brawlers/screen_base.dart';
 import 'package:flutter/material.dart';
 
 main() {
